bb1a7001f4d2abab3126c8928f7f580548dcb762
Betty Cortés <bettycortes@Bettys-MacBook-Pro.local>
2020-10-01 11:17:58 -0500

    Updates

---
+++ precache-manifest.735be2ea12a920632d5f972c76779fff.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef113219de590b0fb3b5970fc75dd93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18a479025110b5f2000e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "16d7d87ba3e5e309459a",
+    "url": "/static/css/main.2e750f02.chunk.css"
+  },
+  {
+    "revision": "18a479025110b5f2000e",
+    "url": "/static/js/2.d8b0285e.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/static/js/2.d8b0285e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d7d87ba3e5e309459a",
+    "url": "/static/js/main.6a210465.chunk.js"
+  },
+  {
+    "revision": "754d88106b8266a615c6",
+    "url": "/static/js/runtime-main.295fde61.js"
+  },
+  {
+    "revision": "8fef3592a4dc7f2c79a816052d12517b",
+    "url": "/static/media/asuq1.8fef3592.jpg"
+  },
+  {
+    "revision": "4a2710062addc8663aca8733a702ec15",
+    "url": "/static/media/asuq2.4a271006.jpg"
+  },
+  {
+    "revision": "6d7616ac191939f7cc4dd93f37dc7741",
+    "url": "/static/media/asuq3.6d7616ac.jpg"
+  },
+  {
+    "revision": "bd372f73751a8041b768a0a022b6986e",
+    "url": "/static/media/capri_caribe1.bd372f73.jpg"
+  },
+  {
+    "revision": "8925e8067541d0d64055a09ef3f34495",
+    "url": "/static/media/capri_caribe2.8925e806.jpg"
+  },
+  {
+    "revision": "8225d1eea861d9b12323ba3448d67b4c",
+    "url": "/static/media/capri_caribe3.8225d1ee.jpg"
+  },
+  {
+    "revision": "f0f44996163e7701e030f8505d441aaf",
+    "url": "/static/media/hacienda_patron1.f0f44996.jpg"
+  },
+  {
+    "revision": "c722a2374da1010227bc77544b611cad",
+    "url": "/static/media/hacienda_patron2.c722a237.jpg"
+  },
+  {
+    "revision": "620d1eab4411a5d82f65d58e1f3a6356",
+    "url": "/static/media/hacienda_patron3.620d1eab.jpg"
+  },
+  {
+    "revision": "f6aa3626a550ba4b094bd300d8b2fad4",
+    "url": "/static/media/landing.f6aa3626.jpeg"
+  },
+  {
+    "revision": "b5519d7ab5c7d143d0c60a716d9a620d",
+    "url": "/static/media/logo.b5519d7a.png"
+  },
+  {
+    "revision": "7cd3141940306168b32050f73ea6d737",
+    "url": "/static/media/manolo.7cd31419.jpg"
+  },
+  {
+    "revision": "fa41552b2fcbe709ceeb2925f9670a23",
+    "url": "/static/media/marquis_cabos1.fa41552b.jpg"
+  },
+  {
+    "revision": "d51e5713a43cc8bc92a79b180aeab8e5",
+    "url": "/static/media/marquis_cabos2.d51e5713.jpg"
+  },
+  {
+    "revision": "e356d51addd21fc46a600812d9421431",
+    "url": "/static/media/marquis_cabos3.e356d51a.jpg"
+  },
+  {
+    "revision": "a273ca0ba7df3bdd436fee14533b1ec1",
+    "url": "/static/media/now_jade1.a273ca0b.jpg"
+  },
+  {
+    "revision": "015b2948f1ac9dee6b1183249849551a",
+    "url": "/static/media/now_jade2.015b2948.jpg"
+  },
+  {
+    "revision": "0b3abcd2f843dde7e653303bd215217c",
+    "url": "/static/media/now_jade3.0b3abcd2.jpg"
+  },
+  {
+    "revision": "afe888e81b9d62d768405daa679dc941",
+    "url": "/static/media/nuevo_guerrero1.afe888e8.jpg"
+  },
+  {
+    "revision": "cb40610550761f74b0883cbfd6ac4a11",
+    "url": "/static/media/nuevo_guerrero2.cb406105.jpg"
+  },
+  {
+    "revision": "e48ad0c0af3d690404b2604ddd2f9fbc",
+    "url": "/static/media/nuevo_guerrero3.e48ad0c0.jpg"
+  },
+  {
+    "revision": "dbb261dbbe00d13be96624029d68e51c",
+    "url": "/static/media/outlet_lerma1.dbb261db.jpg"
+  },
+  {
+    "revision": "8a903ad958abd4a1b1613e4cd609681a",
+    "url": "/static/media/outlet_lerma2.8a903ad9.jpg"
+  },
+  {
+    "revision": "dd4cd520eba616299f40b621d9eeab39",
+    "url": "/static/media/outlet_lerma3.dd4cd520.jpg"
+  },
+  {
+    "revision": "7c9b316a58cb68f63f19160915200322",
+    "url": "/static/media/parque_saki1.7c9b316a.jpg"
+  },
+  {
+    "revision": "f31926529d7446476f5e7f76c4c9b11f",
+    "url": "/static/media/parque_saki2.f3192652.jpg"
+  },
+  {
+    "revision": "f056f9ad99c065668a1823b527d92f39",
+    "url": "/static/media/parque_saki3.f056f9ad.jpg"
+  },
+  {
+    "revision": "29092e8784af58babc7c83c0082b8d01",
+    "url": "/static/media/plaza_bicentenario1.29092e87.jpg"
+  },
+  {
+    "revision": "fe84b9461afee1ad89711172f5cc1ca9",
+    "url": "/static/media/plaza_bicentenario2.fe84b946.jpg"
+  },
+  {
+    "revision": "1958a1226b0bb36a930d9b3378707c38",
+    "url": "/static/media/plaza_bicentenario3.1958a122.jpg"
+  },
+  {
+    "revision": "6adf495c7f9faf332d791e6f2db3b8e4",
+    "url": "/static/media/uvm1.6adf495c.jpg"
+  },
+  {
+    "revision": "6fb2a889b6a86515a20fb55b63de6870",
+    "url": "/static/media/uvm2.6fb2a889.jpg"
+  },
+  {
+    "revision": "2c461a51145e72557f18a40f8a9a0577",
+    "url": "/static/media/uvm3.2c461a51.jpg"
+  },
+  {
+    "revision": "b6a8d0a8f44dc36bb76755972aae5173",
+    "url": "/static/media/ziva_cabos1.b6a8d0a8.jpg"
+  },
+  {
+    "revision": "b12bcdb65480ebd14f3519e2705c76fd",
+    "url": "/static/media/ziva_cabos2.b12bcdb6.jpg"
+  },
+  {
+    "revision": "40fd20486fb31f0bf3ff74711f07dc24",
+    "url": "/static/media/ziva_cabos3.40fd2048.jpg"
+  }
+]);
